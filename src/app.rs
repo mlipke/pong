3fc3847ef0a6e2ceb0c80afdf3d1c14c6931fb63
Matthes Lipke <matthes.lipke@gmail.com>
2016-01-21 15:16:14 +0100

    Now keeping score

--- src/app.rs
+++ src/app.rs
@@ -16,6 +16,7 @@ pub struct App {
     left_paddle: Paddle,
     right_paddle: Paddle,
     state: bool,
+    score: (i32, i32),
     resources: Resources
 }
 
@@ -38,6 +39,7 @@ impl App {
                 rectangle: [0.0, 0.0, 10.0, 40.0],
                 position: (465.0, 160.0)
             },
+            score: (0, 0),
             resources: Resources {
                 font: GlyphCache::new(Path::new("assets/retro_computer.ttf")).unwrap()
             }
@@ -50,6 +52,7 @@ impl App {
         let ref ball = self.ball;
         let ref left_paddle = self.left_paddle;
         let ref right_paddle = self.right_paddle;
+        let ref score = self.score;
         let ref mut resources = self.resources;
 
         gl.draw(args.viewport(), |c, gl| {
@@ -65,10 +68,16 @@ impl App {
 
             rectangle(color::WHITE, ball.rectangle, trans_ball, gl);
 
-            let trans_text = c.transform.trans(192.0, 58.0);
-            let mut text = Text::new(48);
+            let trans_score_left = c.transform.trans(190.0, 55.0);
+            let trans_score_right = c.transform.trans(260.0, 55.0);
+            let mut text = Text::new(50);
             text.color = color::WHITE;
-            text.draw("test", &mut resources.font, &c.draw_state, trans_text, gl);
+
+            text.draw(&format!("{}", score.0),
+                &mut resources.font, &c.draw_state, trans_score_left, gl);
+
+            text.draw(&format!("{}", score.1),
+                &mut resources.font, &c.draw_state, trans_score_right, gl);
         });
     }
 
@@ -96,11 +105,19 @@ impl App {
                 }
             }
 
-            if self.ball.position.0 < 10.0 ||
-               self.ball.position.0 > 470.0 {
+            if self.ball.position.0 < 10.0 {
                    self.ball.position = (240.0, 180.0);
                    self.ball.reference = (240.0, 180.0);
                    self.ball.angle = 0.0;
+                   self.score.1 += 1;
+                   self.toggle_pause();
+            }
+
+            if self.ball.position.0 > 470.0 {
+                   self.ball.position = (240.0, 180.0);
+                   self.ball.reference = (240.0, 180.0);
+                   self.ball.angle = 0.0;
+                   self.score.0 +=1;
                    self.toggle_pause();
             }
 
